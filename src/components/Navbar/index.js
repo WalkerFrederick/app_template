f7ea13cdc93c945d388a37ff1b61435e0f2f0eb4
walker <walker@urdev.xyz>
2019-02-28 17:33:34 -0500

    added hamburger icon

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,7 +6,17 @@ class Navbar extends React.Component {
     render() {
         return (
             <nav>
-
+                <button>
+                    <span/>
+                    <span/>
+                </button>
+                <a href='#' className='logo'>
+                    <h1>APP</h1>                                
+                </a>
+                <button>
+                    <span/>
+                    <span/>
+                </button>
             </nav>
         )
     }
