104942e97d30d7bcdd9eedee41b08f04e2a053ed
walker <walker@urdev.xyz>
2019-02-28 19:47:09 -0500

    added mail icon

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import './index.scss'
 
+import MailIcon from '../Icons/mail';
+
 class Navbar extends React.Component {
 
     render() {
         return (
             <nav>
                 <button>
-                    <span/>
-                    <span/>
+                    <MailIcon/>
                 </button>
-                <a href='#' className='logo'>
+                <a href='#' title='logo' className='logo'>
                     <h1>APP</h1>                                
                 </a>
                 <button>
+                    <span/>
                     <span/>
                     <span/>
                 </button>
