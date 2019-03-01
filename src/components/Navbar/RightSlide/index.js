62b28aa21a7d8dbb7f62a372b3c3bea75ccbb957
walker <walker@urdev.xyz>
2019-02-28 21:27:42 -0500

    added right and left slide components

---
+++ src/components/Navbar/RightSlide/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './index.scss'
+
+import MailIcon from '../Icons/mail';
+
+class RightSlide extends React.Component {
+
+    render() {
+        return (
+            <div className='RightSlide'>
+                
+            </div>
+        )
+    }
+
+}
+
+export default RightSlide;
