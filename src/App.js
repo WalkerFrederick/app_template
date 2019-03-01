9dcb0a03c4a352cc2540a0aae71e5f607b6033dc
walker <walker@urdev.xyz>
2019-03-01 16:48:16 -0500

    added onClick class toggles to navigation buttons

--- src/App.js
+++ src/App.js
@@ -5,10 +5,24 @@ import './App.css';
 import Navbar from './components/Navbar'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      leftSideOpen: false,
+      rightSideOpen: false,
+    }
+
+  }
+  
   render() {
+
+
+
     return (
       <div className="App">
-        <Navbar/>
+        <Navbar leftMenuState={this.toggleLeftSlide} rightMenuState={this.state.rightSideOpen} />
       </div>
     );
   }
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,16 +5,40 @@ import MailIcon from '../Icons/mail';
 
 class Navbar extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            leftActive: false,
+            rightActive: false,
+        }
+    }
+
+    handleLeftClick() {
+        this.setState(
+            {leftActive: !this.state.leftActive,}
+        );
+    }
+
+    handleRightClick() {
+        this.setState(
+            {rightActive: !this.state.rightActive,}
+        );
+    }
+
+
+    
+
     render() {
         return (
             <nav>
-                <button>
+                <button className={this.state.leftActive ? 'left-active' : 'left-hidden'} onClick={this.handleLeftClick.bind(this)}>
                     <MailIcon/>
                 </button>
-                <a href='#' title='logo' className='logo'>
+                <a href='/#' title='logo' className='logo'>
                     <h1>APP</h1>                                
                 </a>
-                <button>
+                <button className={this.state.rightActive ? 'right-active' : 'right-hidden'} onClick={this.handleRightClick.bind(this)}>
                     <span/>
                     <span/>
                     <span/>
