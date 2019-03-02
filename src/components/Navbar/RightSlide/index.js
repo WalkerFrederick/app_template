ad4d5e058f6a18274435a54e8263c07aa264e0a6
walker <walker@urdev.xyz>
2019-03-02 12:45:18 -0500

    added arrows to mobile navigation

--- src/components/Navbar/RightSlide/index.js
+++ src/components/Navbar/RightSlide/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './index.scss'
 
+import ArrowIcon from '../../Icons/arrow'
 
 class RightSlide extends React.Component {
 
@@ -8,9 +9,18 @@ class RightSlide extends React.Component {
         return (
             <div className='RightSlide'>
                 <ul className='main-navigation'>
-                    <li>item 1</li>
-                    <li>item 2</li>
-                    <li>item 3</li>
+                    <li>
+                        <span>item 1</span>
+                        <ArrowIcon/>
+                    </li>
+                    <li>
+                        <span>item 2</span>
+                        <ArrowIcon/>
+                    </li>
+                    <li>
+                        <span>item 3</span>
+                        <ArrowIcon/>
+                    </li>
                 </ul>
                 <ul className='aside-navigation'>
                     <li>item 1</li>
