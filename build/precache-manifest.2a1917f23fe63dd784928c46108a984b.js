602edbe72b0b77a922814564f7d78116640afdff
walker <walker@urdev.xyz>
2019-03-02 23:52:01 -0500

    refactor of navbar sass file

---
+++ build/precache-manifest.2a1917f23fe63dd784928c46108a984b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3342269af39f9401948d",
+    "url": "/appie/static/js/runtime~main.3342269a.js"
+  },
+  {
+    "revision": "6a76da05a347d23325cc",
+    "url": "/appie/static/js/main.6a76da05.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/appie/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "6a76da05a347d23325cc",
+    "url": "/appie/static/css/main.944a13db.chunk.css"
+  },
+  {
+    "revision": "069be9f8d50315aba357233c24fca4d8",
+    "url": "/appie/index.html"
+  }
+];
